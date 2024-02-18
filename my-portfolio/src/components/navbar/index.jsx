d2f23a9de4abd30702811bd07377fbf810c7c93e
Michal_T <michal.trostowiecki@gmail.com>
2024-02-18 19:58:23 +0000

    Add clickable project image, change navbar links

--- my-portfolio/src/components/navbar/index.jsx
+++ my-portfolio/src/components/navbar/index.jsx
@@ -32,48 +32,24 @@ function Navbar() {
             <div className={burgerMenuActive ? "w-full md:block md:w-auto" : "hidden w-full md:block md:w-auto"} id="navbar-dropdown">
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-black md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-black dark:black md:dark:black dark:border-cyan-700">
                 <li>
-                <a href="#" className="block py-2 px-3 text-white bg-cyan-600 rounded md:bg-transparent md:text-cyan-600 md:p-0 md:dark:text-cyan-500 dark:bg-cyan-500 md:dark:bg-transparent" aria-current="page">Home</a>
+                    <a href="#" className="block py-2 px-3 text-white bg-cyan-600 rounded md:bg-transparent md:text-cyan-600 md:p-0 md:dark:text-cyan-500 dark:bg-cyan-500 md:dark:bg-transparent" aria-current="page">
+                        Home
+                    </a>
                 </li>
                 <li>
-                    <button id="dropdownNavbarLink" onClick={handleDropDown} data-dropdown-toggle="dropdownNavbar"
-                            className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-cyan-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-cyan-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-cyan-700 md:dark:hover:bg-transparent"
-                            >
-                            Projects
-                            <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
-                            </svg>
-                    </button>
-                    
-                    <div id="dropdownNavbar" className={
-                        dropDownActive ? 
-                        "z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-cyan-700 dark:divide-cyan-600 absolute"
-                        :
-                        "z-10 font-normal hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-cyan-700 dark:divide-cyan-600 absolute"
-                        }
-                    >
-
-                        <ul className="py-2 text-sm text-white-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
-                        <li>
-                            <a href="#" className="text-white block px-4 py-2 hover:bg-cyan-100 dark:hover:bg-cyan-600 dark:hover:text-white">Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="#" className="text-white block px-4 py-2 hover:bg-gray-100 dark:hover:bg-cyan-600 dark:hover:text-white">Settings</a>
-                        </li>
-                        <li>
-                            <a href="#" className="text-white block px-4 py-2 hover:bg-gray-100 dark:hover:bg-cyan-600 dark:hover:text-white">Earnings</a>
-                        </li>
-                        </ul>
-                        
-                    </div>
+                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-cyan-700 md:p-0 dark:text-white md:dark:hover:text-cyan-500 dark:hover:bg-cyan-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                        Projects
+                    </a>
                 </li>
                 <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-cyan-700 md:p-0 dark:text-white md:dark:hover:text-cyan-500 dark:hover:bg-cyan-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-cyan-700 md:p-0 dark:text-white md:dark:hover:text-cyan-500 dark:hover:bg-cyan-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                        About Me
+                    </a>
                 </li>
                 <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-cyan-700 md:p-0 dark:text-white md:dark:hover:text-cyan-500 dark:hover:bg-cyan-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-                </li>
-                <li>
-                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-cyan-700 md:p-0 dark:text-white md:dark:hover:text-cyan-500 dark:hover:bg-cyan-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-cyan-700 md:p-0 dark:text-white md:dark:hover:text-cyan-500 dark:hover:bg-cyan-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                        Contact
+                    </a>
                 </li>
             </ul>
             </div>
